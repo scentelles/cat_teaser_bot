9bc04b214d2736a62f6474be709b7cee185f1721
Sylvain Centelles <sylvain.centelles@intel.com>
2017-03-06 17:14:45 +0100

    Implement mqtt reconnection loop in separate thread

--- scratch/helper_app.py
+++ scratch/helper_app.py
@@ -1,8 +1,10 @@
+import socket
 import time
 import BaseHTTPServer
 
 import paho.mqtt.client as mqtt
 
+from threading import Thread
 
 import re
 HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
@@ -10,7 +12,6 @@ PORT_NUMBER = 12345 # Maybe set this to 9000.
 
 MQTT_ADDRESS = "192.168.4.2"
 
-value= 0
 client = 0
 
 global currentDistance
@@ -23,17 +24,19 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 		
     def do_GET(s):
         """Respond to a GET request."""
-        global value
         global client
+        global robotIsConnected
         print "Get request received"
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
         
         if (s.path == "/poll"):
-            value += 1
-            s.wfile.write("test " + str(value) + "\n")
-            s.wfile.write("distance " + str(currentDistance) + "\n")
+            if(robotIsConnected):
+                s.wfile.write("distance " + str(currentDistance) + "\n")
+            else:
+                s.wfile.write("_problem The robot is not connected\n")
+            
         if (s.path == "/start"):
             client.publish("robot/start", payload='1', qos=0, retain=False)
 
@@ -60,8 +63,40 @@ def on_message(client, userdata, msg):
     
     if msg.topic == "sensor/distance":
         currentDistance = int(msg.payload)
+
+def on_disconnect(client, userdata, rc):
+    global robotIsConnected
+    print "Disconnected from MQTT server with code: %s" % rc
+    robotIsConnected = False
+    while rc != 0:
+        time.sleep(5)
+        print "Reconnecting..."
+        try:
+            rc = client.reconnect()
+            robotIsConnected = True
+        except socket.error:
+            "Could not connect to Mqtt server\n"
+def mqttClientBody(mqttAddress):
+    global client, robotIsConnected
+    robotIsConnected = False
+    client = mqtt.Client(client_id="ScratchMqtt", clean_session=False)
+    client.on_connect    = on_connect
+    client.on_message    = on_message
+    client.on_disconnect = on_disconnect
     
-  
+    while(robotIsConnected == False):
+        try:
+            client.connect(MQTT_ADDRESS)
+            robotIsConnected = True
+        except socket.error:
+            print "Connection to MQTT on robot side failed. Please check the MQTT the robot address is correct, and the bridge is running.\n"
+            robotIsConnected = False
+    
+    client.subscribe("sensor/distance")    
+
+    client.loop_start()
+
+    
 if __name__ == '__main__':
     print "toto\n"
     server_class = BaseHTTPServer.HTTPServer
@@ -71,15 +106,10 @@ if __name__ == '__main__':
     global client
     global currentDistance
     currentDistance = 0
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
 
-    client.connect(MQTT_ADDRESS)
-
-    client.subscribe("sensor/distance")    
-
-    client.loop_start()
+    # start MQTT client in separate thread to manage reconnection loop 
+    mqttClientThread = Thread(target=mqttClientBody, args=(MQTT_ADDRESS,))
+    mqttClientThread.start()
 
     try:
         httpd.serve_forever()
